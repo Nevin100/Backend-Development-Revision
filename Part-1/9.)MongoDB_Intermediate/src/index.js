0e2b6a1a3c4c609facd592d580183871e238921f
Nevin100 <opopopopop100000@gmail.com>
2025-12-27 19:01:29 +0530

    Completed the Upload

--- Part-1/9.)MongoDB_Intermediate/src/index.js
+++ Part-1/9.)MongoDB_Intermediate/src/index.js
@@ -1,11 +1,12 @@
-import dotenv from dotenv;
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import ConnectDB from "./Utils/db";
 
 const app = express();
 app.use(express.json());
-this.
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server is running on PORT : ${process.env.PORT}`);
+    ConnectDB();
 });
